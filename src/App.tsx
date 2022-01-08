24a32af52aa8006af7e2764c366d6fe596958331
togana <o.togana@gmail.com>
2022-01-08 17:06:23 +0900

    いらないやつ消した

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,6 @@ function App() {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
-      <RenderingNotifier name="outside-Suspense" />
       <Suspense fallback={<p>Loading...</p>}>
         <DataLoader1 />
         <DataLoader2 />
