e1b007a8a41f4c59990b1c178429ff450c7e4f28
togana <o.togana@gmail.com>
2022-01-08 18:00:19 +0900

    Render-as-you-fetch

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ function sleep(ms: number) {
 }
 
 async function fetchData1(): Promise<string> {
-  await sleep(1000);
-  return `Hello, ${(Math.random() * 1000).toFixed(0)}`;
+  const count = Math.random() * 1000;
+  await sleep(count);
+  return `Hello, ${(count).toFixed(0)}`;
 }
 
 const AlwaysSuspend: React.VFC = () => {
@@ -58,16 +59,84 @@ const DataLoader2: React.VFC = () => {
   );
 };
 
+type LoadableState<T> =
+  | {
+      status: "pending";
+      promise: Promise<T>;
+    }
+  | {
+      status: "fulfilled";
+      data: T;
+    }
+  | {
+      status: "rejected";
+      error: unknown;
+    };
+
+export class Loadable<T> {
+  #state: LoadableState<T>;
+  constructor(promise: Promise<T>) {
+    this.#state = {
+      status: "pending",
+      promise: promise.then(
+        (data) => {
+          this.#state = {
+            status: "fulfilled",
+            data,
+          };
+          return data;
+        },
+        (error) => {
+          this.#state = {
+            status: "rejected",
+            error,
+          };
+          throw error;
+        }
+      ),
+    };
+  }
+  getOrThrow(): T {
+    switch (this.#state.status) {
+      case "pending":
+        throw this.#state.promise;
+      case "fulfilled":
+        return this.#state.data;
+      case "rejected":
+        throw this.#state.error;
+    }
+  }
+}
+
+const DataLoader: React.VFC<{
+  data: Loadable<string>;
+}> = ({ data }) => {
+  const value = data.getOrThrow();
+  return (
+    <div>
+      <div>Data is {value}</div>
+    </div>
+  );
+};
+
 function App() {
+  const [data1] = useState(() => new Loadable(fetchData1()));
+  const [data2] = useState(() => new Loadable(fetchData1()));
+  const [data3] = useState(() => new Loadable(fetchData1()));
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
       <Suspense fallback={<p>Loading...</p>}>
-        <DataLoader1 />
-        <DataLoader2 />
+        <DataLoader data={data1} />
+      </Suspense>
+      <Suspense fallback={<p>Loading...</p>}>
+        <DataLoader data={data2} />
+      </Suspense>
+      <Suspense fallback={<p>Loading...</p>}>
+        <DataLoader data={data3} />
       </Suspense>
     </div>
-  )
+  );
 }
 
 export default App
