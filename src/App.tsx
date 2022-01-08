93f246ab75d3b24b92c6b0358ccd5f160941584d
togana <o.togana@gmail.com>
2022-01-08 16:29:01 +0900

    マウント後にサスペンドすれば？（一応成功）

--- src/App.tsx
+++ src/App.tsx
@@ -31,13 +31,21 @@ const RenderingNotifier: React.VFC<{
 };
 
 const DataLoader: React.VFC = () => {
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState<string | null>(null);
-  // dataがまだ無ければローディングを開始する
-  if (data === null) {
+  // ローディングフラグが立っていてdataがまだ無ければローディングを開始する
+  if (loading && data === null) {
     throw fetchData1().then(setData);
   }
   // データがあればそれを表示
-  return <div>Data is {data}</div>;
+  return (
+    <div>
+      <div>Data is {data}</div>
+      <button className="border p-1" onClick={() => setLoading(true)}>
+        load
+      </button>
+    </div>
+  );
 };
 
 function App() {
