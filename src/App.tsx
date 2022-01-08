5c756e981a3cf1d8c09e68a7c1a88388636e2e34
togana <o.togana@gmail.com>
2022-01-08 15:01:59 +0900

    とにかくコンポーネントをサスペンドさせてみる

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css'
+import React from 'react'
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const AlwaysSuspend: React.VFC = () => {
+  throw sleep(1000);
+};
 
 function App() {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
+      <AlwaysSuspend />
     </div>
   )
 }
